e50d05033507c2a13123f4bd9b259f60982216cc
lakshitaa2911 <55542701+lakshitaa2911@users.noreply.github.com>
2021-02-04 22:56:52 +0530

    Create 266A-Stone on the Table.cpp

---
+++ 266A-Stone on the Table.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	string s;
+	cin>>s;
+	int count =0;
+	for(int i=0;i<n;i++){
+		if(s[i]==s[i+1]){
+			count = count +1;
+		}
+		else{
+			continue;
+		}
+	}
+	cout<<count;
+	return 0;
+}
